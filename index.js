7c2607a92dec5c1de78395c2faba1da168bea3ce
MikeeI <MikeeI@users.noreply.github.com>
2021-12-27 21:05:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,16 @@ async function start() {
     await page.goto('https://www.prusaprinters.org/prints/106763-stanley-dewalt-fatmax-deep-pro-organizer/files')
 
     await page.screenshot({ path:"./websitescreenshot.png", fullPage: true})
-    
     const imagepage = await page.goto('https://media.prusaprinters.org/media/prints/106763/stls/1063196_711404c0-5547-49ec-b6e4-9ee219ca92cb/pro_small_2x2.stl')
-    await fs.writeFile('file.gcode', await imagepage.buffer())
+
+    https.get(imagepage, res => {
+            const stream = fs.createWriteStream('somepic.png');
+            res.pipe(stream);
+            stream.on('finish', () => {
+                stream.close();
+            })
+        })
+
 
     await browser.close()
 }
