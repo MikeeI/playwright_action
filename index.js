3124f5a597530bca6196c9524998982b62a182b9
MikeeI <MikeeI@users.noreply.github.com>
2021-12-27 21:07:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ async function start() {
     const page = await browser.newPage()
 
     await page.goto('https://www.prusaprinters.org/prints/106763-stanley-dewalt-fatmax-deep-pro-organizer/files')
+    cookies = await page.cookies();
+    cookie_str = "";
+    for(var i = 0; i < cookies.length; i+=1){
+        a = cookies[i];
+        cookie_str += a.name + "=" + a.value + ";";
+    }
 
-    await page.screenshot({ path:"./websitescreenshot.png", fullPage: true})
-    const imagepage = await page.goto('https://media.prusaprinters.org/media/prints/106763/stls/1063196_711404c0-5547-49ec-b6e4-9ee219ca92cb/pro_small_2x2.stl')
+    
+    const stlurl = 'https://media.prusaprinters.org/media/prints/106763/stls/1063196_711404c0-5547-49ec-b6e4-9ee219ca92cb/pro_small_2x2.stl'
+    
+    request.get({
+        url: stlurl,
+        headers: {
+            "cookie": cookie_str,
+        }
+    }).pipe(fs.createWriteStream("ofname"))
 
-    https.get(imagepage, res => {
-            const stream = fs.createWriteStream('somepic.png');
+    https.get(stlurl, res => {
+            const stream = fs.createWriteStream('stl.stl');
             res.pipe(stream);
             stream.on('finish', () => {
                 stream.close();
