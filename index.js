a6ec86238fe2b72d69bcba8a0ee839ae2b7fdf22
MikeeI <MikeeI@users.noreply.github.com>
2022-01-12 22:46:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ async function start() {
     await page.goto('https://www.prusaprinters.org/prints/117619-alpaca-buddha-llama-buddha/files', { waitUntil: 'networkidle2' })
     await page.waitForSelector('app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn > .filesize')
     await page.click('app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn > .filesize')
+    
+    await page.goto('https://www.prusaprinters.org/prints/117767-l-boxx-bit-hex-holder-lboxx-also-known-as-sortimo/files', { waitUntil: 'networkidle2' })
+    await page.waitForSelector('app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn > .filesize')
+    await page.click('app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn > .filesize')
     await browser.close()    
 }
 
