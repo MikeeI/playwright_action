01d71473ca257965df00152e978321d9eff8fb11
MikeeI <MikeeI@users.noreply.github.com>
2022-01-13 04:45:47 +0100

    Create push_3.js

---
+++ push_3.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs/promises')
+const request = require('request');
+
+async function start() {
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1280, height: 800 })
+    await page.goto('https://www.prusaprinters.org/prints/106763-stanley-dewalt-fatmax-deep-pro-organizer/files', { waitUntil: 'networkidle2' })
+   
+    await page.waitForSelector('app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn > .filesize')
+    await page.click('app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn > .filesize')    
+}
+
+start()
