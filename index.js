16f9736ac141fd2342c45277a6197b4a5e2f3e1e
MikeeI <MikeeI@users.noreply.github.com>
2022-01-13 19:46:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function start() {
     const page = await browser.newPage()
     await page.setViewport({ width: 1280, height: 800 })
     await page.goto('https://www.prusaprinters.org/prints/106763-stanley-dewalt-fatmax-deep-pro-organizer/files', { waitUntil: 'networkidle2' })
-    #await page.goto('https://www.prusaprinters.org/prints/117619-alpaca-buddha-llama-buddha/files', { waitUntil: 'networkidle2' })
+    //await page.goto('https://www.prusaprinters.org/prints/117619-alpaca-buddha-llama-buddha/files', { waitUntil: 'networkidle2' })
     await page.waitForSelector('app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn > .filesize')
     await page.click('app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn > .filesize')    
 }
