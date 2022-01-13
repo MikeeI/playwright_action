cfd6c932087277d1439fa3812c32e77340ef4a1f
MikeeI <MikeeI@users.noreply.github.com>
2022-01-13 04:55:27 +0100

    Update push_2.js

--- push_2.js
+++ push_2.js
@@ -5,7 +5,7 @@ const request = require('request');
 async function start() {
     const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
-    await page.setViewport({ width: 1280, height:
+    await page.setViewport({ width: 1280, height: 800 })
     
     await page.goto('https://www.prusaprinters.org/prints/117767-l-boxx-bit-hex-holder-lboxx-also-known-as-sortimo/files', { waitUntil: 'networkidle2' })
     await page.waitForSelector('app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn > .filesize')
