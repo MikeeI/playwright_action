ced594dcc3b05f521a6be9c8da0884d714bbad53
MikeeI <MikeeI@users.noreply.github.com>
2022-01-14 16:57:46 +0100

    Create prusa_4.js

---
+++ prusa_4.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs/promises')
+const request = require('request');
+
+async function start(url) {
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
+    const navigationPromise = page.waitForNavigation()
+
+    await page.goto(url)
+    await page.setViewport({ width: 2560, height: 1600 })
+    await navigationPromise
+    //await page.waitForSelector('.max-width-md > app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn')
+    //await page.click('.max-width-md > app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn')
+
+    await browser.close()
+}
+
+start('https://www.prusaprinters.org/prints/118424-vacuumroborockroobma-laundry-stopper-and-run-over-/files')
