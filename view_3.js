012c282a813ad045996a8330abbe520fa5e10577
MikeeI <MikeeI@users.noreply.github.com>
2022-01-17 20:22:53 +0100

    Update view_3.js

--- view_3.js
+++ view_3.js
@@ -10,10 +10,12 @@ async function start(url) {
     await page.goto(url)
     await page.setViewport({ width: 2560, height: 1600 })
     await navigationPromise
-    //await page.waitForSelector('.max-width-md > app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn')
-    //await page.click('.max-width-md > app-market-downloads:nth-child(1) > .first-item > .download-wrapper > .btn')
-
-    await browser.close()
+    await page.waitForSelector('.detail-content > .row > .col-auto > .views > .number')
+    let element = await page.$('.detail-content > .row > .col-auto > .views > .number')
+    const text = await page.evaluate(element => element.textContent, element);
+    console.log(text)
+    setTimeout(() => { browser.close(); }, 3000);
 }
 
-start('https://www.prusaprinters.org/prints/106763-stanley-dewalt-fatmax-deep-pro-organizer/files')
+
+start('https://www.prusaprinters.org/prints/117767-l-boxx-bit-hex-holder-lboxx-also-known-as-sortimo')
