1033abb4b77713d0f51d881e3a1295d618597f6a
MikeeI <MikeeI@users.noreply.github.com>
2022-01-17 22:22:14 +0100

    Create json.js

---
+++ json.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer')
+//const fs = require('fs/promises')
+const request = require('request');
+
+const fs = require('fs');
+
+let json_db = fs.readFileSync('db.json');
+let rows = JSON.parse(json_db);
+console.log(rows[0]);
+
+//rows.forEach(element => console.log(element));
+for (var i = 0; i < rows.length; i++){
+  document.write("<br><br>array index: " + i);
+  var obj = rows[i];
+  for (var key in obj){
+    var value = obj[key];
+    document.write("<br> - " + key + ": " + value);
+  }
+}
+
+
+
