af5d7b481779542ee8dbbc4ed1c6661fa8e368ec
MikeeI <MikeeI@users.noreply.github.com>
2022-01-17 22:21:44 +0100

    Update and rename login.js to json.js

--- json.js
+++ json.js
@@ -9,7 +9,14 @@ let rows = JSON.parse(json_db);
 console.log(rows[0]);
 
 //rows.forEach(element => console.log(element));
-
+for (var i = 0; i < rows.length; i++){
+  document.write("<br><br>array index: " + i);
+  var obj = rows[i];
+  for (var key in obj){
+    var value = obj[key];
+    document.write("<br> - " + key + ": " + value);
+  }
+}
 
 async function like_url(url) {
     /*
