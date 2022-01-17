2dd6ea5fe1a417257c04c85b06d427dc1eae2f00
MikeeI <MikeeI@users.noreply.github.com>
2022-01-17 20:18:15 +0100

    Update view_1.js

--- view_1.js
+++ view_1.js
@@ -10,12 +10,12 @@ async function start(url) {
     await page.goto(url)
     await page.setViewport({ width: 2560, height: 1600 })
     await navigationPromise
-    await delay(4000);
     await page.waitForSelector('.detail-content > .row > .col-auto > .views > .number')
     let element = await page.$('.detail-content > .row > .col-auto > .views > .number')
     const text = await page.evaluate(element => element.textContent, element);
     console.log(text)
-    await browser.close()
+    setTimeout(() => { browser.close(); }, 3000);
+
 }
 
 start('https://www.prusaprinters.org/prints/106763-stanley-dewalt-fatmax-deep-pro-organizer')
