fc37b846b1cf814ceb2363a3df9dd1a0ff1eee94
MikeeI <MikeeI@users.noreply.github.com>
2022-01-17 22:25:40 +0100

    Update json.js

--- json.js
+++ json.js
@@ -5,11 +5,11 @@ console.log(rows[0]);
 
 //rows.forEach(element => console.log(element));
 for (var i = 0; i < rows.length; i++){
-  print("array index: " + i);
+  console.log("array index: " + i);
   var obj = rows[i];
   for (var key in obj){
     var value = obj[key];
-    print(" - " + key + ": " + value);
+    console.log(" - " + key + ": " + value);
   }
 }
 
