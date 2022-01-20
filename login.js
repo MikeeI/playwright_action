d10c2f2b8a751f735673cd93792ec77ee13fbd3a
MikeeI <MikeeI@users.noreply.github.com>
2022-01-20 18:40:01 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,29 +1,58 @@
 const puppeteer = require('puppeteer')
 //const fs = require('fs/promises')
 const request = require('request');
-
 const fs = require('fs');
 
 let json_db = fs.readFileSync('db.json');
 let rows = JSON.parse(json_db);
-console.log(rows[0]);
 
-//rows.forEach(element => console.log(element));
-for (var i = 0; i < rows.length; i++){
-  //document.write("<br><br>array index: " + i);
-  rows[i]["like"]="true"
-  var obj = rows[i];
-  for (var key in obj){
-    var value = obj[key];
-    console.log(obj["like"])
-    //document.write("<br> - " + key + ": " + value);
-  }
+account = "booths.revise_0n@icloud.com"
+auth_token = "x0L9LfgZPFPd0NXUPISzfJL3kH1eHC"
+
+for (var i = 0; i < rows.length; i++) {
+
+    console.log("array index: " + i);
+    if (rows[i]["account"] == account) {
+        if (rows[i]["like"] == false) {
+            console.log("Like: False | Execute Like")
+            rows[i]["like"] = true;
+            like_url(rows[i]["url"],auth_token)
+        }
+    }
 }
 
 fs.writeFileSync('db2.json', JSON.stringify(rows, null, 2));
 
-async function like_url(url) {
+
+/*
+//rows.forEach(element => console.log(element));
+for (var i = 0; i < rows.length; i++) {
+    console.log("array index: " + i);
+
+    var element = rows[i];
+    if (element["like"] == false) 
+    {
+        console.log("Like: False | Execute Like")
+        rows[i]["like"]=true;
+    }
     /*
+    for (var key in element) {
+        var value = element[key];
+        console.log(element);
+        if (element["like"] == false) {
+            console.log("Like: False | Execute Like")
+            
+        }
+        //console.log(" - " + key + ": " + value);
+    }
+    */
+    
+}
+
+fs.writeFileSync('db2.json', JSON.stringify(rows, null, 2));
+
+async function like_url(url,auth_token) {
+    
     const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
     const navigationPromise = page.waitForNavigation()
@@ -34,7 +63,7 @@ async function like_url(url) {
         token => {
             localStorage.clear();
             localStorage.setItem('auth_token', token);
-        }, 'x0L9LfgZPFPd0NXUPISzfJL3kH1eHC');
+        }, auth_token);
     
     
     await page.goto(url, { waitUntil: 'networkidle2' })
@@ -83,6 +112,6 @@ async function like_url(url) {
     //setTimeout(() => { browser.close(); }, 6000);
 }
 
-like_url('https://www.prusaprinters.org/prints/117767-l-boxx-bit-hex-holder-lboxx-also-known-as-sortimo')
-like_url('https://www.prusaprinters.org/prints/118424-vacuumroborockroobma-laundry-stopper-and-run-over-')
+//like_url('https://www.prusaprinters.org/prints/117767-l-boxx-bit-hex-holder-lboxx-also-known-as-sortimo')
+//like_url('https://www.prusaprinters.org/prints/118424-vacuumroborockroobma-laundry-stopper-and-run-over-')
 
