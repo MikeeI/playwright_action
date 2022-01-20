7fd22f490dbefcd470936378b3fc8af67b59233d
MikeeI <MikeeI@users.noreply.github.com>
2022-01-20 18:47:33 +0100

    Update login.js

--- login.js
+++ login.js
@@ -49,8 +49,6 @@ for (var i = 0; i < rows.length; i++) {
     
 
 
-fs.writeFileSync('db2.json', JSON.stringify(rows, null, 2));
-
 async function like_url(url,auth_token) {
     
     const browser = await puppeteer.launch({ headless: true })
@@ -80,38 +78,8 @@ async function like_url(url,auth_token) {
     await page.screenshot({
         path: 'screenshot-2.png',
     });
-   
-    
-    
-    /*
-    await page.waitForSelector('.d-flex > .nav-block > .navbar-nav > .navbar-item:nth-child(6) > .btn')
-    await page.click('.d-flex > .nav-block > .navbar-nav > .navbar-item:nth-child(6) > .btn')
-
-    await navigationPromise
-
-    await page.waitForSelector('#id_email')
-    await page.click('#id_email')
-    await page.type('#id_email', 'platinum2589@gmail.com')
-
-    await page.waitForSelector('#id_password')
-    await page.click('#id_password')
-    await page.type('#id_password', 'Wyi3JVjGmfGwzTtqNFNU2CTP4xH4avXJ')
-
-
-    await page.waitForSelector('.justify-content-center > .main-content > #recaptcha_form > .form-group > .btn')
-    await page.click('.justify-content-center > .main-content > #recaptcha_form > .form-group > .btn')
-
-    await navigationPromise
-    
-    await page.goto('https://www.prusaprinters.org/prints/106763-stanley-dewalt-fatmax-deep-pro-organizer')
-
-    await page.waitForSelector('.d-none > .w-100 > .btn > div > .mr-1 > div > svg > .like-fill > path')
-    await page.click('.d-none > .w-100 > .btn > div > .mr-1 > div > svg > .like-fill > path')
-
-    */
+        
     setTimeout(() => { browser.close(); }, 6000);
 }
 
-//like_url('https://www.prusaprinters.org/prints/117767-l-boxx-bit-hex-holder-lboxx-also-known-as-sortimo')
-//like_url('https://www.prusaprinters.org/prints/118424-vacuumroborockroobma-laundry-stopper-and-run-over-')
 
