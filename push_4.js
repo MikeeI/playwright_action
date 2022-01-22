77f54422e3d3e38aa612397704c311e4f9982316
MikeeI <MikeeI@users.noreply.github.com>
2022-01-22 03:53:47 +0100

    Update push_4.js

--- push_4.js
+++ push_4.js
@@ -10,12 +10,18 @@ async function start(url) {
     await page.goto(url)
     await page.setViewport({ width: 2560, height: 1600 })
     await navigationPromise
-    await page.waitForSelector('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
-    await page.click('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
+    //await page.waitForSelector('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
+    //await page.click('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
+    
+    await page.waitForSelector('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
+    await page.click('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
+    
     await page.waitForSelector('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
     let element = await page.$('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
+    
     const text = await page.evaluate(element => element.textContent, element);  
     console.log(text)
     setTimeout(() => { browser.close(); }, 6000);
 }
+
 start('https://www.prusaprinters.org/prints/118424-vacuumroborockroobma-laundry-stopper-and-run-over-/files')
