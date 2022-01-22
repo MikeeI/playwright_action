cc1fb73e106655a7fee8362af4c02672a26b20e3
MikeeI <MikeeI@users.noreply.github.com>
2022-01-22 04:07:52 +0100

    Update push_4.js

--- push_4.js
+++ push_4.js
@@ -15,22 +15,22 @@ async function start(url) {
     await page.click('#onetrust-accept-btn-handler')
     
     setTimeout(() => { 
-
-        await page.waitForSelector('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
-        await page.click('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
-
-        await page.waitForSelector('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
-        let element = await page.$('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
-
-        const text = await page.evaluate(element => element.textContent, element);  
-        console.log(text)
-
-        await page.screenshot({
-            path: 'screenshot-2.png',
-        });
-        setTimeout(() => { browser.close(); }, 6000);
+        console.log("Timeout");
+        
     }, 6000);
-    
+    await page.waitForSelector('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
+    await page.click('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
+
+    await page.waitForSelector('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
+    let element = await page.$('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
+
+    const text = await page.evaluate(element => element.textContent, element);  
+    console.log(text)
+
+    await page.screenshot({
+        path: 'screenshot-2.png',
+    });
+    setTimeout(() => { browser.close(); }, 6000);
     
   
 
