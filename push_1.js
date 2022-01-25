9af4f6222f6b128f10fe7660f446d22bbe438dd1
MikeeI <MikeeI@users.noreply.github.com>
2022-01-25 15:03:44 +0100

    Update push_1.js

--- push_1.js
+++ push_1.js
@@ -16,6 +16,7 @@ async function start(url) {
     
     await page.waitForSelector('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
     await page.click('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
+    
     await page.waitForSelector('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
     let element = await page.$('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
     const text = await page.evaluate(element => element.textContent, element);  
