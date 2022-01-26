e36088f2aee200ce0998b2f7d79bd1e641485b02
MikeeI <MikeeI@users.noreply.github.com>
2022-01-26 18:49:18 +0100

    Update push_3.js

--- push_3.js
+++ push_3.js
@@ -10,11 +10,17 @@ async function start(url) {
     await page.goto(url)
     await page.setViewport({ width: 2560, height: 1600 })
     await navigationPromise
-    await page.waitForSelector('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
-    await page.click('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
+
     
-    //await page.waitForSelector('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
-    //await page.click('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
+    await page.waitForTimeout(2000); // wait for 5 seconds
+    await page.waitForSelector('#onetrust-accept-btn-handler')
+    await page.click('#onetrust-accept-btn-handler')
+    await page.waitForTimeout(2000); // wait for 5 seconds
+    
+    //await page.waitForSelector('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
+    //await page.click('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
+    await page.waitForSelector('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
+    await page.click('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
     
     await page.waitForSelector('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
     let element = await page.$('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
@@ -22,7 +28,13 @@ async function start(url) {
     const text = await page.evaluate(element => element.textContent, element);  
     console.log(text)
     setTimeout(() => { browser.close(); }, 6000);
+    
+    await page.screenshot({
+        path: 'screenshot-2.png',
+    });
 }
+
+
 
 
 start('https://www.prusaprinters.org/prints/117619-alpaca-buddha-llama-buddha/files')
