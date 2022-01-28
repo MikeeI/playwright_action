b931267328fd6b0792688ed9a12506158c6c8ec4
MikeeI <MikeeI@users.noreply.github.com>
2022-01-28 04:09:54 +0100

    Update view_2.js

--- view_2.js
+++ view_2.js
@@ -3,19 +3,18 @@ const fs = require('fs/promises')
 const request = require('request');
 
 async function start(url) {
-    const browser = await puppeteer.launch({ headless: true })
+    const browser = await puppeteer.launch({ args: [
+      '--proxy-server=socks5://localhost:40000',
+    ],headless: true })
     const page = await browser.newPage()
     const navigationPromise = page.waitForNavigation()
 
     await page.goto(url)
     await page.setViewport({ width: 2560, height: 1600 })
     await navigationPromise
-    await page.waitForSelector('.detail-content > .row > .col-auto > .views > .number')
-    let element = await page.$('.detail-content > .row > .col-auto > .views > .number')
-    const text = await page.evaluate(element => element.textContent, element);
-    console.log(text)
+    
     setTimeout(() => { browser.close(); }, 3000);
 }
 
 
-start('https://www.prusaprinters.org/prints/117619-alpaca-buddha-llama-buddha')
+start('https://www.thingiverse.com/thing:4894102')
