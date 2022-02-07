62e692c905aa668e9403fa7f2aa13d2aed5c7153
MikeeI <MikeeI@users.noreply.github.com>
2022-02-07 18:32:27 +0100

    Update view_2.js

--- view_2.js
+++ view_2.js
@@ -3,16 +3,21 @@ const fs = require('fs/promises')
 const request = require('request');
 
 async function start(url) {
-    const browser = await puppeteer.launch({ headless: true })
+    const browser = await puppeteer.launch({ args: [
+      '--proxy-server=socks5://localhost:40000',
+    ],headless: true })
     const page = await browser.newPage()
     const navigationPromise = page.waitForNavigation()
 
     await page.goto(url)
     await page.setViewport({ width: 2560, height: 1600 })
     await navigationPromise
-    
-    setTimeout(() => { browser.close(); }, 3000);
+    await page.waitForSelector('.detail-content > .row > .col-auto > .views > .number')
+    let element = await page.$('.detail-content > .row > .col-auto > .views > .number')
+    const text = await page.evaluate(element => element.textContent, element);
+    console.log(text)
+    setTimeout(() => { browser.close(); }, 6000);
 }
 
 
-start('https://www.thingiverse.com/thing:4894102')
+start('https://www.prusaprinters.org/prints/128293-heavy-duty-pencil-case-with-sharpener')
