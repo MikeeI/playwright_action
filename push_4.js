d639edd09ca1cec3c6ae54368f8ee132049145ec
MikeeI <MikeeI@users.noreply.github.com>
2022-02-14 18:16:08 +0100

    Update push_4.js

--- push_4.js
+++ push_4.js
@@ -3,26 +3,28 @@ const fs = require('fs/promises')
 const request = require('request');
 
 async function start(url, multiple_files) {
-    const browser = await puppeteer.launch({ headless: true })
+    const browser = await puppeteer.launch({
+        headless: true
+    })
     const page = await browser.newPage()
     const navigationPromise = page.waitForNavigation()
 
     await page.goto(url)
-    await page.setViewport({ width: 2560, height: 1600 })
+    await page.setViewport({
+        width: 2560,
+        height: 1600
+    })
     await navigationPromise
-    
+
     await page.waitForSelector('#onetrust-accept-btn-handler')
     await page.click('#onetrust-accept-btn-handler')
-    
+
     await page.waitForTimeout(5000); // wait for 5 seconds
-    
-    if (multiple_files == true)
-    {
+
+    if (multiple_files == true) {
         await page.waitForSelector('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
         await page.click('app-market-downloads > .download-item > .download-wrapper > .btn > .fa')
-    }
-    else
-    {
+    } else {
         await page.waitForSelector('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
         await page.click('app-market-downloads:nth-child(1) > .download-item > .download-wrapper > .btn > .fa')
     }
@@ -30,17 +32,15 @@ async function start(url, multiple_files) {
     await page.waitForSelector('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
     let element = await page.$('.detail-grid > .detail-content > .download-btn > .d-flex > .download-count')
 
-    const text = await page.evaluate(element => element.textContent, element);  
+    const text = await page.evaluate(element => element.textContent, element);
     console.log(text)
 
     await page.screenshot({
         path: 'screenshot-2.png',
     });
-    setTimeout(() => { browser.close(); }, 6000);
-    
-  
-
-        
+    setTimeout(() => {
+        browser.close();
+    }, 6000);
 }
 
 start('https://www.prusaprinters.org/prints/117619-alpaca-buddha-llama-buddha/files', false)
